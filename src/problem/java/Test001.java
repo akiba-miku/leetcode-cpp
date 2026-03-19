704968d2a717d1058f4df4cbfd06780cf79497e9
Junxjx <2013137851@qq.com>
2026-03-19 20:16:51 +0800

    测试

---
+++ src/problem/java/Test001.java
@@ -0,0 +1,17 @@
+// 注意包名要和目录结构对应
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test001 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
