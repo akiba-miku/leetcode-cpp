31d9ccf4573cbf815ef52e88ea8e092e14da1b12
makism <furina.see.fun@gmail.com>
2026-03-21 17:56:15 +0800

    lc:[CPP]->0053_Maximum_Subarray.cpp

---
+++ problem/cpp/0053_Maximum_Subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    // 滚动数组的方案自己想
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        int max_val = nums[0];
+        for(int i=1;i<n;++i){
+            dp[i]=max(dp[i-1]+nums[i],nums[i]);
+            max_val = max(max_val, dp[i]);
+        }
+        return max_val;
+    }
+};
