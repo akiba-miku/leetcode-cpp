de9a6cd6281b11e434510d7f0f46afac7f9b53e2
makism <furina.see.fun@gmail.com>
2026-03-31 00:04:19 +0800

    0104_Maxinum_Depth_of_Binary_Tree.cpp

---
+++ problem/cpp/0104_Maximun_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <queue>
+#include <algorithm>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// 基本递归， 层序遍历， dfs+stack
+class Solution
+{
+public:
+    int maxDepth1(TreeNode *root)
+    {
+        return root != nullptr ? max({maxDepth1(root->left), maxDepth1(root->right)}) + 1
+                    : 0;
+    }
+
+    int maxDepth(TreeNode *root) {
+        if(root == nullptr) return 0;
+        queue<TreeNode*> q;
+        int depth = 0;
+        q.push(root);
+        while(!q.empty()) {
+            int size = q.size();
+            for(int i=0;i<size;i++) {
+                auto node = q.front();
+                q.pop();
+                if(node->left) {
+                    q.push(node->left);
+                }
+                if(node->right) {
+                    q.push(node->right);
+                }
+            }
+            depth++;
+        }
+
+        return depth;
+    }
+
+};
